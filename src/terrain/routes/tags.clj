610d61044d8248a6058378e1ffb9443bceedfc10
Dennis Roberts <dennis@cyverse.org>
2017-11-06 15:30:55 -0700

    CORE-9067: allowed emacs to reindent terrain.routes.tags

--- src/terrain/routes/tags.clj
+++ src/terrain/routes/tags.clj
@@ -8,22 +8,22 @@
 (defn secured-tag-routes
   []
   (util/optional-routes
-    [#(and (config/filesystem-routes-enabled) (config/metadata-routes-enabled))]
+   [#(and (config/filesystem-routes-enabled) (config/metadata-routes-enabled))]
 
-    (GET "/filesystem/entry/:entry-id/tags" [entry-id]
-         (tags/list-attached-tags entry-id))
+   (GET "/filesystem/entry/:entry-id/tags" [entry-id]
+     (tags/list-attached-tags entry-id))
 
-    (PATCH "/filesystem/entry/:entry-id/tags" [entry-id type :as {body :body}]
-           (tags/handle-patch-file-tags entry-id type body))
+   (PATCH "/filesystem/entry/:entry-id/tags" [entry-id type :as {body :body}]
+     (tags/handle-patch-file-tags entry-id type body))
 
-    (GET "/tags/suggestions" [contains limit]
-         (tags/suggest-tags contains limit))
+   (GET "/tags/suggestions" [contains limit]
+     (tags/suggest-tags contains limit))
 
-    (POST "/tags/user" [:as {body :body}]
-          (tags/create-user-tag body))
+   (POST "/tags/user" [:as {body :body}]
+     (tags/create-user-tag body))
 
-    (PATCH "/tags/user/:tag-id" [tag-id :as {body :body}]
-           (tags/update-user-tag tag-id body))
+   (PATCH "/tags/user/:tag-id" [tag-id :as {body :body}]
+     (tags/update-user-tag tag-id body))
 
-    (DELETE "/tags/user/:tag-id" [tag-id]
-            (tags/delete-user-tag tag-id))))
+   (DELETE "/tags/user/:tag-id" [tag-id]
+     (tags/delete-user-tag tag-id))))
