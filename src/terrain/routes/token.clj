345ce349f50c5a9078da70839d8757797737c64b
Sarah Roberts <sarahr@cyverse.org>
2019-01-17 14:13:26 -0700

    added instructions for using the /token endpoint from the command line

--- src/terrain/routes/token.clj
+++ src/terrain/routes/token.clj
@@ -15,7 +15,21 @@
         "the Logout button in the `Basic authenitcation` section of authorization window. Second, click the"
         "Authorization button again and type the word \"Bearer\" followed by a single space in the Value text"
         "box of the `Api key authorization` section of the window. Paste in the access token from this endpoint's"
-        "response body then click the Authorize button underneath the Value text box."]
+        "response body then click the Authorize button underneath the Value text box."
+        ""
+        "You can use `curl` and `jq`, which is available from [the jq web site](https://stedolan.github.io/jq/), to"
+        "obtain an access token from the command line. The easiest way to do this on Unix-like operating systems"
+        "is to define an environment variable containing the authorization header:"
+        ""
+        "```"
+        "export AUTH_HEADER=\"Authorization: Bearer $(curl -su username https://de.cyverse.org/terrain/token | jq -r .access_token)\""
+        "```"
+        "Once you have the authorization header stored in an environment variable, you can include it in calls to"
+        "other Terrain endpoints:"
+        ""
+        "```"
+        "curl -sH \"$AUTH_HEADER\" \"https://de.cyverse.org/terrain/apps?search=word\""
+        "```"]
        (string/join "\n")))
 
 (defn token-routes
