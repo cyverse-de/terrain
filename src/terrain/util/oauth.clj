154265978ccf83d548d25b3f916f5460dacc910d
Sarah Roberts <sarahr@cyverse.org>
2019-01-22 17:22:20 -0700

    CORE-23: fix a problem with some exceptions being caught in the wrong place

--- src/terrain/util/oauth.clj
+++ src/terrain/util/oauth.clj
@@ -3,6 +3,7 @@
   (:require [clojure.tools.logging :as log]
             [clojure-commons.error-codes :as ce]
             [clojure-commons.response :as resp]
+            [ring.util.http-response :as http-resp]
             [terrain.clients.oauth :as oauth]
             [terrain.util.config :as config]))
 
@@ -34,20 +35,26 @@
         (handler request)
         (resp/forbidden "You are not in one of the admin groups.")))))
 
+(defn get-oauth-profile
+  [token]
+  (try+
+   (doto (oauth/get-profile token)
+     (validate-oauth-profile))
+   (catch [:status 401] _
+     (log/warn (:throwable &throw-context) "Invalid or expired OAuth token.")
+     (resp/unauthorized "Invalid or expired OAuth token."))
+   (catch [:type :validation] _
+     (resp/forbidden (.getMessage (:throwable &throw-context))))
+   (catch Object _
+     (log/error (:throwable &throw-context) "Unexpected exception.")
+     (resp/error-response http-resp/internal-server-error
+                          ce/ERR_UNCHECKED_EXCEPTION
+                          "Received an unexpected exception."))))
+
 (defn validate-oauth-token
   [handler token-fn]
   (fn [request]
-    (try+
-     (if-let [token (token-fn request)]
-       (let [profile (oauth/get-profile token)]
-         (validate-oauth-profile profile)
-         (handler (assoc request :oauth-profile profile)))
-       (resp/unauthorized "No OAuth token found."))
-     (catch [:status 401] _
-       (log/warn (:throwable &throw-context) "Invalid or expired OAuth token.")
-       (resp/unauthorized "Invalid or expired OAuth token."))
-     (catch [:type :validation] _
-       (resp/forbidden (.getMessage (:throwable &throw-context))))
-     (catch Object _
-       (log/error (:throwable &throw-context) "Unexpected exception.")
-       (resp/error-response ce/ERR_UNCHECKED_EXCEPTION "Received an unexpected exception.")))))
+    (if-let [token (token-fn request)]
+      (let [profile (get-oauth-profile token)]
+        (handler (assoc request :oauth-profile profile)))
+      (resp/unauthorized "No OAuth token found."))))
