b7499c45d025142f19377bdaa97f847c78b5ced4
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-03-08 13:18:00 -0700

    Use the new badge schemas for NewBadge and UpdateBadge

--- src/terrain/routes/analyses.clj
+++ src/terrain/routes/analyses.clj
@@ -1,7 +1,7 @@
 (ns terrain.routes.analyses
   (:require [compojure.api.sweet :refer [describe]]
             [common-swagger-api.schema :refer [context GET PUT POST DELETE]]
-            [common-swagger-api.schema.badges :refer [Badge NewBadge]]
+            [common-swagger-api.schema.badges :refer [Badge NewBadge UpdateBadge]]
             [common-swagger-api.schema.apps :refer [AnalysisSubmission]]
             [ring.util.http-response :refer [ok]]
             [terrain.util :refer [optional-routes]]
@@ -29,7 +29,7 @@
       (ok (analyses/get-badge id)))
 
     (PUT "/:id" [id]
-      :body         [badge NewBadge]
+      :body         [badge UpdateBadge]
       :return       Badge
       :summary      "Modifies an existing badge"
       :description  "Modifies an existing badge, allowing the caller to change
@@ -37,13 +37,13 @@
       (ok (analyses/update-badge id badge)))
 
     (POST "/" []
-      :body         [submission AnalysisSubmission]
+      :body         [badge NewBadge]
       :return       Badge
       :summary      "Adds a badge to the database"
       :description  "Adds a badge and corresponding submission information to the
       database. The username passed in should already exist. A new UUID will be
       assigned and returned."
-      (ok (analyses/add-badge submission)))
+      (ok (analyses/add-badge badge)))
 
     (DELETE "/:id" [id]
       :return        DeletionResponse
