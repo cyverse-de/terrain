3168bd3c59dd3437bdaed7cd688d8d07e908f3fd
Ashley Ramsey <aramsey@iplantcollaborative.org>
2019-04-09 12:26:22 -0700

    Add context and tag for search routes

--- src/terrain/routes/search.clj
+++ src/terrain/routes/search.clj
@@ -1,7 +1,7 @@
 (ns terrain.routes.search
   "the routing code for search-related URL resources"
   (:use [clojure-commons.error-codes :only [missing-arg-response]]
-        [common-swagger-api.schema :only [GET POST]])
+        [common-swagger-api.schema]
   (:require [terrain.auth.user-attributes :as user]
             [terrain.services.search :as search]
             [terrain.clients.search :as c-search]
@@ -15,12 +15,16 @@
   (util/optional-routes
     [config/search-routes-enabled]
 
-    (GET "/filesystem/search-documentation" [:as req]
-      (util/controller req c-search/get-data-search-documentation))
-    (POST "/filesystem/search" [:as req]
-      (util/controller req c-search/do-data-search :params :body))
+    (context "/filesystem" []
+       :tags ["filesystem"]
 
-    (GET "/filesystem/index" [q tags & opts]
-      (if (or q tags)
-        (search/search (:shortUsername user/current-user) q tags opts)
-        (missing-arg-response "`q` or `tags`")))))
+       (GET "/search-documentation" [:as req]
+            (util/controller req c-search/get-data-search-documentation))
+       (POST "/search" [:as req]
+             (util/controller req c-search/do-data-search :params :body))
+
+       (GET "/index" [q tags & opts]
+            (if (or q tags)
+              (search/search (:shortUsername user/current-user) q tags opts)
+              (missing-arg-response "`q` or `tags`")))
+       )))
