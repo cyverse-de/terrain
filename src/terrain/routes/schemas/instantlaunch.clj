da5a801c8fa5f3f06e0f0c30400754e49ab1c176
John Wregglesworth <wregglej@cyverse.org>
2020-12-08 10:03:58 -0700

    Add schema for instant launches

---
+++ src/terrain/routes/schemas/instantlaunch.clj
@@ -0,0 +1,25 @@
+(ns terrain.routes.schemas.instantlaunch
+  (:use [common-swagger-api.schema :only [describe NonBlankString]]
+        [schema.core :only [defschema Any Keyword optional-key]])
+  (:import [java.util UUID]))
+
+(defschema InstantLaunch
+  {:id              (describe UUID "The UUID of the instant launch")
+   :quick_launch_id (describe UUID "The UUID for the quick launch used for the instant launch")
+   :added_by        (describe String "The username of the user that created the instant launch")
+   :added_on        (describe String "The date and time when the instant launch was created")})
+
+(defschema InstantLaunchSelector
+  {:pattern    (describe String "The pattern used to match against the file. Interpretation is defined by the 'kind' field")
+   :kind       (describe String "The kind of selector. Freeform for now")
+   :default    (describe InstantLaunch "The default instant launch for the file")
+   :compatible (describe [InstantLaunch] "Instant launches that are compatible with the file, but are not the default")})
+
+(defschema InstantLaunchMapping
+  {(describe Keyword "Instant Launch pattern key") (describe InstantLaunchSelector "Instant Launch pattern definition")})
+
+(defschema DefaultInstantLaunchMapping
+  {:id      (describe UUID "The UUID of the default instant launch mapping")
+   :version (describe String "The format version of the instant launch mapping")
+   :mapping (describe InstantLaunchMapping "The set of patterns use to match files to instant launches")})
+
