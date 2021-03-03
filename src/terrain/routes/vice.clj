c35ae0ae41177bfd70bd79ad19552ed39b142f49
John Wregglesworth <wregglej@cyverse.org>
2021-03-03 12:40:09 -0700

    Forgot that /admin was already in the URLs for the VICE stuff. Removed the redundant reference to it

--- src/terrain/routes/vice.clj
+++ src/terrain/routes/vice.clj
@@ -16,77 +16,76 @@
    (context "/vice" []
      :tags ["admin-vice"]
 
-     (context "/admin" []
-       (GET "/resources" []
-         :query [filter vice-schema/FilterParams]
+     (GET "/resources" []
+       :query [filter vice-schema/FilterParams]
+       :return vice-schema/FullResourceListing
+       :summary "List Kubernetes resources deployed in the cluster"
+       :description "Lists all Kubernetes resources associated with an analysis running in the cluster."
+       (ok (vice/admin-get-resources filter)))
+
+     (GET "/async-data" []
+       :query [params vice-schema/AsyncDataParams]
+       :return vice-schema/AsyncData
+       :summary "Get data that is generated asynchronously"
+       :description "Get data for the VICE analysis that is generated asynchronously"
+       (ok (vice/async-data params)))
+
+     (context "/analyses" []
+       (context "/:analysis-id" []
+         :path-params [analysis-id :- vice-schema/AnalysisID]
+
+         (GET "/time-limit" []
+           :return vice-schema/TimeLimit
+           :summary "Get current time limit"
+           :description "Gets the current time limit set for the analysis"
+           (ok (vice/admin-get-time-limit analysis-id)))
+
+         (POST "/time-limit" []
+           :return vice-schema/TimeLimit
+           :summary "Extend the time limit"
+           :description "Extends the time limit for the analysis by 3 days"
+           (ok (vice/admin-set-time-limit analysis-id)))
+
+         (POST "/save-and-exit" []
+           :summary "Upload outputs and exit"
+           :description "Terminates the analysis after uploading the output files to the data store"
+           (ok (vice/admin-cancel-analysis analysis-id)))
+
+         (POST "/exit" []
+           :summary "Exit without saving"
+           :description "Terminates the analysis without uploading files to the data store"
+           (ok (vice/admin-exit analysis-id)))
+
+         (POST "/save-output-files" []
+           :summary "Upload files without exiting"
+           :description "Uploads output files for the analysis to the data store without terminating the analysis"
+           (ok (vice/admin-save-output-files analysis-id)))
+
+         (POST "/download-input-files" []
+           :summary "Download input files"
+           :description "Downloads input files to the analysis container without changing the status of the analysis"
+           (ok (vice/admin-download-input-files analysis-id)))
+
+         (GET "/external-id" []
+           :return vice-schema/ExternalIDResponse
+           :summary "Get external UUID"
+           :description "Returns the external UUID associated with the analysis. VICE analyses only have a single external UUID"
+           (ok (vice/admin-external-id analysis-id)))))
+
+     (context "/:host" []
+       :path-params [host :- vice-schema/Host]
+
+       (GET "/url-ready" []
+         :return vice-schema/URLReady
+         :summary vice-schema/URLReadySummary
+         :description vice-schema/URLReadyDescription
+         (ok (vice/admin-url-ready host)))
+
+       (GET "/description" []
          :return vice-schema/FullResourceListing
-         :summary "List Kubernetes resources deployed in the cluster"
-         :description "Lists all Kubernetes resources associated with an analysis running in the cluster."
-         (ok (vice/admin-get-resources filter)))
-
-       (GET "/async-data" []
-         :query [params vice-schema/AsyncDataParams]
-         :return vice-schema/AsyncData
-         :summary "Get data that is generated asynchronously"
-         :description "Get data for the VICE analysis that is generated asynchronously"
-         (ok (vice/async-data params)))
-
-       (context "/analyses" []
-         (context "/:analysis-id" []
-           :path-params [analysis-id :- vice-schema/AnalysisID]
-
-           (GET "/time-limit" []
-             :return vice-schema/TimeLimit
-             :summary "Get current time limit"
-             :description "Gets the current time limit set for the analysis"
-             (ok (vice/admin-get-time-limit analysis-id)))
-
-           (POST "/time-limit" []
-             :return vice-schema/TimeLimit
-             :summary "Extend the time limit"
-             :description "Extends the time limit for the analysis by 3 days"
-             (ok (vice/admin-set-time-limit analysis-id)))
-
-           (POST "/save-and-exit" []
-             :summary "Upload outputs and exit"
-             :description "Terminates the analysis after uploading the output files to the data store"
-             (ok (vice/admin-cancel-analysis analysis-id)))
-
-           (POST "/exit" []
-             :summary "Exit without saving"
-             :description "Terminates the analysis without uploading files to the data store"
-             (ok (vice/admin-exit analysis-id)))
-
-           (POST "/save-output-files" []
-             :summary "Upload files without exiting"
-             :description "Uploads output files for the analysis to the data store without terminating the analysis"
-             (ok (vice/admin-save-output-files analysis-id)))
-
-           (POST "/download-input-files" []
-             :summary "Download input files"
-             :description "Downloads input files to the analysis container without changing the status of the analysis"
-             (ok (vice/admin-download-input-files analysis-id)))
-
-           (GET "/external-id" []
-             :return vice-schema/ExternalIDResponse
-             :summary "Get external UUID"
-             :description "Returns the external UUID associated with the analysis. VICE analyses only have a single external UUID"
-             (ok (vice/admin-external-id analysis-id)))))
-
-       (context "/:host" []
-         :path-params [host :- vice-schema/Host]
-
-         (GET "/url-ready" []
-           :return vice-schema/URLReady
-           :summary vice-schema/URLReadySummary
-           :description vice-schema/URLReadyDescription
-           (ok (vice/admin-url-ready host)))
-
-         (GET "/description" []
-           :return vice-schema/FullResourceListing
-           :summary vice-schema/DescriptionSummary
-           :description vice-schema/AdminDescriptionDescription
-           (ok (vice/admin-get-description-by-host host))))))))
+         :summary vice-schema/DescriptionSummary
+         :description vice-schema/AdminDescriptionDescription
+         (ok (vice/admin-get-description-by-host host)))))))
 
 (defn vice-routes
   []
