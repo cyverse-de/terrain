8dd3a621395aec1d1c6c143bab8b9dc855a02026
John Wregglesworth <wregglej@cyverse.org>
2022-02-25 14:48:14 -0700

    Change UUID to String for data usage schema

--- src/terrain/routes/schemas/data_usage_api.clj
+++ src/terrain/routes/schemas/data_usage_api.clj
@@ -1,15 +1,14 @@
 (ns terrain.routes.schemas.data-usage-api
   (:require [common-swagger-api.schema :refer [describe]]
-            [schema.core :refer [defschema optional-key maybe]])
-  (:import [java.util UUID]))
+            [schema.core :refer [defschema optional-key maybe]]))
 
 (def UserCurrentDataSummary "Get the most recent summary of a user's data usage")
 
 (def UserCurrentDataDescription "Uses the current date to get the most recent record of data usage for the currently logged-in user")
 
 (defschema UserCurrentDataTotal
-  {:id            (describe UUID "The UUID assigned to the current reading")
-   :user_id       (describe UUID "The UUID assigned to the user the reading applies to")
+  {:id            (describe String "The UUID assigned to the current reading")
+   :user_id       (describe String "The UUID assigned to the user the reading applies to")
    :username      (describe String "The username of the user the reading applies to")
    :total         (describe Long "The total number of bytes the user is consuming in the Data Store")
    :time          (describe String "The date of the reading")
