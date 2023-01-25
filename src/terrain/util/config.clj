4b6325028b5a62df01a1aafdf81785fdc519f895
John Wregglesworth <wregglej@cyverse.org>
2023-01-25 14:49:03 -0700

    Add NATS settings to config.clj

--- src/terrain/util/config.clj
+++ src/terrain/util/config.clj
@@ -598,6 +598,31 @@
   [props config-valid configs]
   "terrain.dashboard-aggregator.base-uri" "http://dashboard-aggregator")
 
+(cc/defprop-optstr nats-urls
+  "A comma separated list of NATS connection URLs."
+  [props config-valid configs]
+  "terrain.nats.urls" "tls://nats-0.nats,tls://nats-1.nats,tls://nats-2.nats,tls://nats-3.nats")
+
+(cc/defprop-optstr nats-tls-dir
+  "The path to the directory containing the client TLS files needed for connecting to NATS"
+  [props config-valid configs]
+  "terrain.nats.tls.dir" "/etc/nats/tls")
+
+(cc/defprop-optstr nats-tls-key-fname
+  "The filename of the TLS key used for connecting to NATS. Must be present in nats-tls-dir"
+  [props config-valid configs]
+  "terrain.nats.tls.key" "tls.pkcs8")
+
+(cc/defprop-optstr nats-tls-crt-fname
+  "The filename of the TLS key used for connecting to NATS. Must be present in nats-tls-dir"
+  [props config-valid configs]
+  "terrain.nats.tls.crt" "tls.crt")
+
+(cc/defprop-optstr nats-tls-ca-fname
+  "The filename of the TLS CA crt used for connecting to NATS. Must be present in nats-tls-dir"
+  [props config-valid configs]
+  "terrain.nats.tls.ca" "ca.crt")
+
 (def async-tasks-client
   (memoize #(async-tasks-client/new-async-tasks-client (async-tasks-base-url))))
 
