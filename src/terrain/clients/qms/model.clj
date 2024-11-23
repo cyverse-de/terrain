a6d33521877224e588b9f9bccf23203ba6a188a6
Sarah Roberts <sonoran.sarah@gmail.com>
2024-11-22 17:14:15 -0700

    CORE-2016: finished writing the QMS model conversion functions and began adding unit tests

--- src/terrain/clients/qms/model.clj
+++ src/terrain/clients/qms/model.clj
@@ -4,13 +4,19 @@
             AddAddonRequest
             Addon
             AddonRate
-            AssociateByUuids
-            ByUuid
+            AssociateByUUIDs
+            ByUUID
             NoParamsRequest
             ResourceType
+            Plan
+            PlanRate
+            QMSUser
+            QuotaDefault
+            Subscription
             SubscriptionAddon
             UpdateAddonRequest
-            UpdateSubscriptionAddonRequest]))
+            UpdateSubscriptionAddonRequest
+            Usage]))
 
 (defn resource-type-from-map
   [m]
@@ -31,7 +37,7 @@
     (when (contains? m :uuid)
       (.setUuid builder (:uuid m)))
     (when (contains? m :rate)
-      (.setUuid builder (:rate m)))
+      (.setRate builder (:rate m)))
     (when (contains? m :effective_date)
       (.setEffectiveDate builder (time/protobuf-timestamp (:effective_date m))))
     (.build builder)))
@@ -87,17 +93,109 @@
 
 (defn by-uuid-request-from-map
   [m]
-  (let [builder (ByUuid/newBuilder)]
+  (let [builder (ByUUID/newBuilder)]
     (when (contains? m :uuid)
-      (.setUuid builder (:uuid m)))))
+      (.setUuid builder (:uuid m)))
+    (.build builder)))
 
 (defn associate-by-uuids-from-map
   [m]
-  (let [builder (AssociateByUuids/newBuilder)]
+  (let [builder (AssociateByUUIDs/newBuilder)]
     (when (contains? m :parent_uuid)
       (.setParentUuid builder (:parent_uuid m)))
     (when (contains? m :child_uuid)
-      (.setChildUuid builder (:child_uuid m)))))
+      (.setChildUuid builder (:child_uuid m)))
+    (.build builder)))
+
+(defn user-from-map
+  [m]
+  (let [builder (QMSUser/newBuilder)]
+    (when (contains? m :uuid)
+      (.setUuid builder (:uuid m)))
+    (when (contains? m :username)
+      (.setUsername builder (:username m)))
+    (.build builder)))
+
+(defn quota-default-from-map
+  [m]
+  (let [builder (QuotaDefault/newBuilder)]
+    (when (contains? m :uuid)
+      (.setUuid builder (:uuid m)))
+    (when (contains? m :quota_value)
+      (.setQuotaValue builder (:quota_value m)))
+    (when (contains? m :resource_type)
+      (.setResourceType builder (resource-type-from-map (:resource_type m))))
+    (when (contains? m :effective_date)
+      (.setEffectiveDate builder (time/protobuf-timestamp (:effective_date m))))
+    (.build builder)))
+
+(defn plan-rate-from-map
+  [m]
+  (let [builder (PlanRate/newBuilder)]
+    (when (contains? m :uuid)
+      (.setUuid builder (:uuid m)))
+    (when (contains? m :rate)
+      (.setRate builder (:rate m)))
+    (when (contains? m :effective_date)
+      (.setEffectiveDate builder (time/protobuf-timestamp (:effective_date m))))
+    (.build builder)))
+
+(defn plan-from-map
+  [m]
+  (let [builder (Plan/newBuilder)]
+    (when (contains? m :uuid)
+      (.setUuid builder (:uuid m)))
+    (when (contains? m :name)
+      (.setName builder (:name m)))
+    (when (contains? m :description)
+      (.setDescription builder (:description m)))
+    (when (contains? m  :plan_quota_defaults)
+      (.setPlanQuotaDefaults (into-array QuotaDefault (map quota-default-from-map (:plan_quota_defaults m)))))
+    (when (contains? m :plan_rates)
+      (.setPlanRates builder (into-array PlanRate (map plan-rate-from-map (:plan_rates m)))))
+    (.build builder)))
+
+(defn usage-from-map
+  [m]
+  (let [builder (Usage/newBuilder)]
+    (when (contains? m :uuid)
+      (.setUuid builder (:uuid m)))
+    (when (contains? m :usage)
+      (.setUsage builder (:usage m)))
+    (when (contains? m :subscription_id)
+      (.setSubscriptionId builder (:subscription_id m)))
+    (when (contains? m :resource_type)
+      (.setResourceType builder (resource-type-from-map (:resource_type m))))
+    (when (contains? m :created_by)
+      (.setCreatedby builder (:created_by m)))
+    (when (contains? m :crated_at)
+      (.setCreatedAt builder (time/protobuf-timestamp (:created_at m))))
+    (when (contains? m :last_modified_by)
+      (.setLastModifiedBy builder (:last_modified_by m)))
+    (when (contains? m :last_modified_at)
+      (.setLastModifiedAt builder (time/protobuf-timestamp (:last_modified_at m))))
+    (.build builder)))
+
+(defn subscription-from-map
+  [m]
+  (let [builder (Subscription/newBuilder)]
+    (when (contains? m :uuid)
+      (.setUuid builder (:uuid m)))
+    (when (contains? m :effective_start_date)
+      (.setEffectiveStartDate builder (time/protobuf-timestamp (:effective_start_date m))))
+    (when (contains? m :effective_end_date)
+      (.setEffectiveEndDate builder (time/protobuf-timestamp (:effective_end_date m))))
+    (when (contains? m :user)
+      (.setUser builder (user-from-map (:user m))))
+    (when (contains? m :plan)
+      (.setPlan builder (plan-from-map (:plan m))))
+    (when (contains? m :usages)
+      (.setUsages builder (into-array Usage (map usage-from-map (:usages m)))))
+    (when (contains? m :paid)
+      (.setPaid builder (:paid m)))
+    (when (contains? m :plan_rate)
+      (.setPlanRate builder (plan-rate-from-map (:plan_rate m))))
+    (.build builder)))
 
 (defn subscription-addon-from-map
   [m]
@@ -106,13 +204,27 @@
       (.setUuid builder (:uuid m)))
     (when (contains? m :addon)
       (.setAddon builder (addon-from-map (:addon m))))
-    ;; TODO: finish implementing this function.
-    ))
+    (when (contains? m :subscription)
+      (.setSubscription builder (subscription-from-map (:subscription m))))
+    (when (contains? m :amount)
+      (.setAmount builder (:amount m)))
+    (when (contains? m :paid)
+      (.setPaid builder (:paid m)))
+    (when (contains? m :addon_rate)
+      (.setAddonRate builder (addon-rate-from-map (:addon_rate m))))
+    (.build builder)))
 
 (defn update-subscription-addon-request-from-map
   [m]
   (let [builder (UpdateSubscriptionAddonRequest/newBuilder)]
     (when (contains? m :subscription_addon)
       (.setSubscriptionAddon builder (subscription-addon-from-map (:subscription_addon m))))
-    ;; TODO: finish implementing this function
-    ))
+    (when (contains? m :update_addon_id)
+      (.setUpdateAddonId builder (:update_addon_id m)))
+    (when (contains? m :update_subscription_id)
+      (.setUpdateSubscriptionId builder (:update_subscription_id m)))
+    (when (contains? m :update_amount)
+      (.setUpdateAmount builder (:update_amount m)))
+    (when (contains? m :update_paid)
+      (.setUpdatePaid builder (:update_paid m)))
+    (.build builder)))
