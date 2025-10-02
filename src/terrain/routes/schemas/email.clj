381cdb72a7dffd25d9af9e706ffdd6694ef11216
Sarah Roberts <sonoran.sarah@gmail.com>
2025-10-01 17:11:50 -0700

    CORE-2086: added support for cc and bcc headers

--- src/terrain/routes/schemas/email.clj
+++ src/terrain/routes/schemas/email.clj
@@ -1,7 +1,7 @@
 (ns terrain.routes.schemas.email
   (:require
    [common-swagger-api.schema :as s]
-   [schema.core :refer [Keyword]]))
+   [schema.core :refer [Keyword optional-key]]))
 
 (def SendEmailSummary "Send an Email")
 
@@ -11,9 +11,11 @@
   {:status (s/describe s/NonBlankString "The status of the request")})
 
 (def SendEmailRequestBody
-  {:to        (s/describe s/NonBlankString "The email address to send the message to")
-   :from_addr (s/describe s/NonBlankString "The email address of the message sender")
-   :from_name (s/describe s/NonBlankString "The name of the message sender")
-   :subject   (s/describe s/NonBlankString "The message subject")
-   :template  (s/describe s/NonBlankString "The name of the email template to use")
-   :values    (s/describe {Keyword s/NonBlankString} "The values to plug into the email template")})
+  {:to                 (s/describe s/NonBlankString "The email address to send the message to")
+   (optional-key :cc)  (s/describe [s/NonBlankString] "Email addresses to send courtecy copies to")
+   (optional-key :bcc) (s/describe [s/NonBlankString] "Email addresses to send blind courtecy copies to")
+   :from_addr          (s/describe s/NonBlankString "The email address of the message sender")
+   :from_name          (s/describe s/NonBlankString "The name of the message sender")
+   :subject            (s/describe s/NonBlankString "The message subject")
+   :template           (s/describe s/NonBlankString "The name of the email template to use")
+   :values             (s/describe {Keyword s/NonBlankString} "The values to plug into the email template")})
